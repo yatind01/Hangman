edb57acf10fe71b9bfcf3d957f640d1a4c362cad
YATIN DHINGRA <yatind01@gmail.com>
2017-07-05 16:32:56 +0530

    Update gameletter.java

--- Hangman/src/gameletter.java
+++ Hangman/src/gameletter.java
@@ -1,8 +1,8 @@
 
 public class gameletter {
 	public String word(){
-		String[] words = {"HANGMAN", "FERRARI", "HARMONIUM", "MERCEDES", "WRESTLING", "LIBERTY", "HARMONY", "CONSTITUTION", "LETTERBOX", "ACTIVATION", "HIMALAYA"};
-		int index = (int)(Math.random() * 11);
+		String[] words = {"HANGMAN", "FERRARI", "HARMONIUM", "MERCEDES", "WRESTLING", "LIBERTY", "HARMONY", "CONSTITUTION", "LETTERBOX", "ACTIVATION", "HIMALAYA", "NUMBSKULL", "ZOMBIE", "MYSTERIOUS", "ADVENTUROUS", "POLKADOTS", "QUADCORE", "FLUFFINESS"};
+		int index = (int)(Math.random() * 18);
 		String s = words[index];
 		return s;
 	}
